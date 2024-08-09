41cacd65f31ac422a4d21538a6ba65de5915c4a8
Abhik Ray <abhik.raya01@gmail.com>
2024-08-09 15:14:35 +0530

    added some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -8,28 +8,59 @@ const App: FunctionComponent = () => {
   return (
     <div className="app">
       <Navbar />
-      <SpineTimeline 
-        points={['2018', '2022', '2024', '2032', '2077']} 
+      {/* <SpineTimeline
+        points={["2018", "2022", "2024", "2032", "2077"]}
         selectedPoint={3}
-        selectedPointProgress={50} 
-      />
-      {/* <Typewriter
-        options={{
-          strings: [
-            "Welcome",
-            "स्वागतम्‌",
-            "স্বাগতম",
-            "Willkommen",
-            "いらっしゃいませ",
-          ],
-          autoStart: true,
-          loop: true,
-          delay: "natural",
-          deleteSpeed: "natural",
-          wrapperClassName: "typewriter-wrapper",
-          cursorClassName: "typewriter-cursor",
-        }}
+        selectedPointProgress={50}
       /> */}
+      <div className="main-body">
+        <div className="intro-text">
+          <Typewriter
+            options={{
+              strings: [
+                "Welcome",
+                "स्वागतम्‌",
+                "স্বাগতম",
+                "Willkommen",
+                "いらっしゃいませ",
+              ],
+              autoStart: true,
+              loop: true,
+              delay: "natural",
+              deleteSpeed: "natural",
+              wrapperClassName: "typewriter-wrapper",
+              cursorClassName: "typewriter-cursor",
+            }}
+          />
+        </div>
+        <div>
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+            enim dicta saepe aperiam, ullam rem voluptatum consequatur et quis
+            rerum maxime cumque non quaerat ut repellendus eius ipsam
+            dignissimos? Aspernatur.
+          </p>
+        </div>
+        <div>
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+            enim dicta saepe aperiam, ullam rem voluptatum consequatur et quis
+            rerum maxime cumque non quaerat ut repellendus eius ipsam
+            dignissimos? Aspernatur.
+          </p>
+        </div>
+        <div>
+          <h3>Lorem Ipsum</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt
+            enim dicta saepe aperiam, ullam rem voluptatum consequatur et quis
+            rerum maxime cumque non quaerat ut repellendus eius ipsam
+            dignissimos? Aspernatur.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
