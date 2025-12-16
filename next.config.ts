22558e83c56f82cd2b98718b8edd368c3a06f570
Abhik Ray <abhik.raya01@gmail.com>
2025-12-16 22:18:19 +0530

    trying a method

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  output: "export",
+import type { NextConfig } from 'next';
 
-  basePath: "/my-portfolio",
-  assetPrefix: "/my-portfolio",
-
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
+
+export default nextConfig;
